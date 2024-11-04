40d08870e7c37bd45f28a08e130686b8bb6fd2af
Yumin Chu <yuminc03@gmail.com>
2024-11-04 23:05:08 +0900

    Chore: 잘못된 파일 위치 수정

--- ProgrammersSwiftCodingTest/MonthlyCodeChallengeSeason2/AddingMinusPlus.swift
+++ ProgrammersSwiftCodingTest/MonthlyCodeChallengeSeason2/AddingMinusPlus.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 // 음양 더하기
-func solution(_ absolutes: [Int], _ signs: [Bool]) -> Int {
+func addingMinusPlus(_ absolutes: [Int], _ signs: [Bool]) -> Int {
     return (0 ..< absolutes.count).map { signs[$0] ? absolutes[$0] : absolutes[$0] * -1 }.reduce(0, +)
 }
