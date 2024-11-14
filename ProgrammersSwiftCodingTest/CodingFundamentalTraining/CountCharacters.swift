84056d5871c9a198271df9409e46ae2a08447dc7
Yumin Chu <yuminc03@gmail.com>
2024-11-14 17:58:33 +0900

    Feat: 문자 개수 세기 (중간저장)

---
+++ ProgrammersSwiftCodingTest/CodingFundamentalTraining/CountCharacters.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+// 문자 개수 세기
+func countCharacters(_ my_string: String) -> [Int] {
+    let characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//    characters.map { $0.uppercased() } + characters.map { $0.lowercased() }.enumerated()
+//    return my_string.map { $0} Array(repeating: 0, count: characters.count * 2).map { }
+    return []
+}
