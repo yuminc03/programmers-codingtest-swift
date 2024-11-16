23b3f387da8e8afeb6e04564ee2fd3378209824b
Yumin Chu <yuminc03@gmail.com>
2024-11-16 16:07:26 +0900

    Feat: 그림 확대

--- ProgrammersSwiftCodingTest/CodingFundamentalTraining/ZoomInPicture.swift
+++ ProgrammersSwiftCodingTest/CodingFundamentalTraining/ZoomInPicture.swift
@@ -2,5 +2,5 @@ import Foundation
 
 // 그림 확대
 func zoomInPicture(_ picture: [String], _ k: Int) -> [String] {
-    return []
+    return picture.flatMap { Array(repeating: $0.map { String(repeating: $0, count: k) }.joined(), count: k) }
 }
